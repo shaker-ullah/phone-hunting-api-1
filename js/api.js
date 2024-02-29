42bf9335b2ee01a9c91429ea5cf33b0622406260
shaker-ullah <shakeru449@gmail.com>
2024-02-29 20:43:05 +0600

    added

--- js/api.js
+++ js/api.js
@@ -1,42 +1,52 @@
-const loadDate = async (search) => {
-    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${search}`)
-    const data = await res.json()
-    const phones = data.data
-    displayData(phones)
+const loadData = async (searchText, isShow) => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+  const data = await res.json()
+  displayData(data.data, isShow)
 }
 
+const displayData = (phones, isShow) => {
+  const divContainer = document.getElementById('card-container')
+  divContainer.textContent = ''
 
-const displayData = (phones) => {
-    console.log(phones);
-    const mainContainer = document.getElementById('card-container')
-    mainContainer.textContent = ''
-    phones.forEach(phone => {
-        console.log(phone);
-        const cardContainer = document.createElement('div')
-        cardContainer.classList = `card  bg-gray-300 shadow-xl`
-        cardContainer.innerHTML = `
-  <figure><img src="${phone.image}" alt="Shoes" /></figure>
-  <div class="card-body">
-    <h2 class="card-title">${phone.phone_name}</h2>
-    <p>${phone.brand}</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+  const showAllContainer = document.getElementById('show-all-container')
+  if (phones.length > 6) {
+    showAllContainer.classList.remove('hidden')
+  }
+  else {
+    showAllContainer.classList.add('hidden')
+  }
+
+  phones = phones.slice(0, 6)
+
+  phones.forEach((phone) => {
+    // console.log(phone);
+
+    const cardContainer = document.createElement('div')
+    cardContainer.classList = `card w-96 bg-base-100 shadow-xl`
+    cardContainer.innerHTML = `
+    <figure><img src="${phone.image}" alt="Shoes" /></figure>
+    <div class="card-body">
+      <h2 class="card-title">${phone.phone_name}</h2>
+      <p>${phone.brand}</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
     </div>
-  </div>
-</div>
-        `
-        mainContainer.appendChild(cardContainer)
-
-    })
-}
-
-
-const handleSearch = () => {
-    const searchField = document.getElementById('search-input')
-    const searchText = searchField.value;
-    loadDate(searchText)
-    searchField.value = ""
+`
+    divContainer.appendChild(cardContainer)
+  })
 
 }
 
 
+
+const handleSearch = (isShow) => {
+  const searchInput = document.getElementById('search-input');
+  const searchText = searchInput.value;
+  console.log(searchText);
+  loadData(searchText, isShow)
+}
+
+const handleShowAll = () => {
+  handleSearch(true)
+}
